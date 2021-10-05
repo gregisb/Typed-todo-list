811021eb06fa6db797a53be6a68990f0d9281abd
Guilherme Regis <guilhermeb.regis@gmail.com>
2021-10-05 14:33:10 -0300

    typed tasks, map items

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -5,3 +5,19 @@ export const Container = styled.div`
     color: #797a8a;
     min-height: 100vh
 `;
+
+export const Area = styled.div`
+    margin: auto;
+    max-width: 980px;
+    padding: 10px;
+`;
+
+export const Header = styled.h1`
+    margin: 0;
+    pádding: 0;
+    color: white;
+    text-align: center;
+    border-bottom: 1px solid #444;
+    padding-bottom: 20px;
+    
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
+import React, { useState } from 'react';
+import {Item} from './types/item'
+
 import * as C from './App.styles'
 
 const App = () => {
+  const [list, setList] = useState<Item[]>([
+    {
+      id: 1,
+      name: 'Comprar pão',
+      done: false
+    },
+    {
+      id: 2,
+      name: 'Comprar bolo',
+      done: false
+    },
+  ])
+
+
   return(
     <C.Container>
-      ...
+      <C.Area>
+        <C.Header>
+          Todo List
+        </C.Header>
+
+        {list.map((item, index) => (
+          <div>{item.name}</div>
+        ))}
+      </C.Area>
     </C.Container>
   );
 }
